8ce1424b4e98b0c3f41a9a4374139e26db98277f
smtamme <stellamariitamme@gmail.com>
2023-09-13 12:50:15 +0300

    basics.js added findIndex and Sum

---
+++ lessons/23-09-13/basics.js
@@ -0,0 +1,14 @@
+const arr = [13, 9, 10, 5, 3, 6]
+//console.log(arr.indexOf(6))
+
+function findIndex (arr, element) {
+    return arr.indexOf(element)
+}
+
+console.log(findIndex(arr, 9))
+
+function sum (a, b) {
+    return a + b
+}
+
+console.log(sum(1, 2))
